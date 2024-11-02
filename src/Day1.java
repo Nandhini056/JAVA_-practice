43642277019e7eb41c00d71e0355439ac540223e
nandhini056 <nandhinikatrajmtp@gmail.com>
2024-11-02 13:27:21 +0530

    java practice DAY1

---
+++ src/Day1.java
@@ -0,0 +1,12 @@
+public class Day1 {
+    public static void main(String[] args){
+        int num = 10;
+        double val = 3.14;
+        char alpha = 'A';
+        boolean bool =true;
+        System.out.println(num);
+        System.out.println(val);
+        System.out.println(alpha);
+        System.out.println(bool);
+    }
+}
